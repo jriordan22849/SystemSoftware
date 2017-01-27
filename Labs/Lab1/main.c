328371e3097d1935a33ee3ccc5f187fffa0146d2
Jonathan Riordan <c13432152@mydit.ie>
2017-01-27 14:49:32 +0000

    Update

--- Labs/Lab1/main.c
+++ Labs/Lab1/main.c
@@ -3,14 +3,18 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "areaOfRectangle.h"
 #include "areaOfCirlce.h"
 
 int main(int argc, char **argv) {
-    int num, num2;
-    sscanf(argv[2],"%d",&num);
-    sscanf(argv[3],"%d",&num2);
 
-    areaOfRectangle(num,num2);
-    //areaOfCircle(8,9);
+    if(strcmp(argv[1],"Rectangle") == 0) {
+        areaOfRectangle(atoi(argv[2]),atoi(argv[3]));
+    } else if(strcmp(argv[1],"Circle") == 0) {
+        areaOfCircle(atoi(argv[2]),atoi(argv[3]));
+    }
+
+
 }
